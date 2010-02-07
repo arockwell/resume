e0ad84a0cf4b44d95e761e1ce80295f3deb59b97
Alex Rockwell <arockwell@gmail.com>
2010-02-06 22:00:32 -0500

    Added support to generate html file. Also fixed a bug where periods were not added to each skill line.

--- generate_resume.rb
+++ generate_resume.rb
@@ -3,53 +3,99 @@
 require 'rubygems'
 require 'json'
 
-def generate_plain_text(resume)
-	result = ""
+def generate_output(resume, formatter)
 	personal_info = resume['personal_info']
-	result = personal_info['name'] + "\n"
-	result += personal_info['email'] + "\n"
-	result += personal_info['phone'] + "\n"
-	result += "\n"
+	result = formatter.para(personal_info['name'])
+	result += formatter.para(personal_info['email']) 
+	result += formatter.para(personal_info['phone'])
+	result += formatter.break_line
 
 	experience = resume['experience']
-	result += "EXPERIENCE\n"
-	experience.each { |exp| result += exp + "\n" }
-	result += "\n"
+	result += formatter.heading("EXPERIENCE")
+	experience.each { |exp| result += formatter.para(exp) }
+	result += formatter.break_line 
 
-	result += "WORK HISTORY\n"
+	result += formatter.heading("WORK HISTORY")
 	work_history = resume['work_history']
 	work_history.each do |job|
-		result += job["basic_info"] + "\n"
-		result += job["description"] + "\n"
-		result += "\n"
+		result += formatter.para(job["basic_info"])
+		result += formatter.para(job["description"])
+		result += formatter.break_line
 	end
 
-	result += "SKILLS\n"
+	result += formatter.heading("SKILLS")
 	skills = resume['skills']
 	skills.each do |skill|
 		if skill.has_key?('some')
-			result += skill['skill'] + "\n"
-			result += "  - Significant experience with " + skill['significant'] + ".\n"
-			result += "  - Some experience with " + skill['some'] + ".\n"
+			result += formatter.para(skill['skill'])
+			sig = "Significant experience with " + skill['significant'] + "."
+			some = "Some experience with " + skill['some'] + "."
+			result += formatter.list([sig, some])
 		else
-			result += skill['skill'] + ": "
-			result += "Significant experience with " + skill['significant'] + ".\n"
+			sig = skill['skill'] + ": "
+			sig += "Significant experience with " + skill['significant']  + "."
+			result += formatter.para(sig)
 		end
 	end
-	result += "\n"
+	result += formatter.break_line
 
 	education = resume['education']
-	result += "EDUCATION\n"
-	result += education['location'] + "\n"
-	result += education['degree'] + "\n"
-	result += "\n"
+	result += formatter.heading("EDUCATION")
+	result += formatter.para(education['location'])
+	result += formatter.para(education['degree'])
+	result += formatter.break_line
 
 
 	references = resume['references']
-	result += "REFERENCES\n"
-	references.each { |ref| result += ref + "\n" }
+	result += formatter.heading("REFERENCES")
+	references.each { |ref| result += formatter.para(ref) }
 
 	return result
+end
+
+class PlainTextFormatter
+	def heading(string)
+		return string.upcase + "\n"
+	end
+
+	def para(string)
+		return string + "\n"
+	end
+
+	def list(strings) 
+		result = ""
+		strings.each do |string|
+			result += "  - " + string + "\n"
+		end
+		return result
+	end
+
+	def break_line()
+		return "\n"
+	end
+end
+
+class HtmlFormatter
+	def heading(string)
+		return "<h1>" + string.upcase + "</h1>\n"
+	end
+
+	def para(string)
+		return "<p>" + string + "</p>\n"
+	end
+
+	def list(strings)
+		result = "<ul>\n"
+		strings.each do |string|
+			result += "<li>" + string + "</li>\n"
+		end
+		result += "</ul>\n"
+		return result
+	end
+
+	def break_line()
+		return "<br />\n"
+	end
 end
 
 json_resume = "resume.js"
@@ -57,5 +103,14 @@ resume = ""
 File.open(json_resume).each_line { |line| resume += line }
 resume = JSON.parse(resume)
 
-resume_txt = generate_plain_text(resume)
+formatter = PlainTextFormatter.new
+resume_txt = generate_output(resume, formatter)
+txt_file = File.new("README", "w")
+txt_file.puts(resume_txt)
 print resume_txt
+
+
+formatter = HtmlFormatter.new
+resume_html = generate_output(resume, formatter)
+html_file = File.new("resume.html", 'w')
+html_file.puts(resume_html)
